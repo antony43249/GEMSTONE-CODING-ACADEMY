37e0fd58c3c06e3f2ae3e602228f72661c64c6af
antony43249 <odadiomoiti@gmail.com>
2025-08-29 20:37:54 +0300

    Update urls.py

--- gemstone_academy/urls.py
+++ gemstone_academy/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('courses/', include('courses.urls')),
+    path('enrollments/', include('enrollments.urls')),
+    path('projects/', include('projects.urls')),
+    path('payments/', include('payments.urls')),
+    path('marketing/', include('marketing.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ]
+
