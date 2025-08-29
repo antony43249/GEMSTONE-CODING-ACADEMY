526b1d300984e5e3d88965f309945279e9557059
antony43249 <odadiomoiti@gmail.com>
2025-08-29 20:47:06 +0300

    Update models.py

--- enrollments/models.py
+++ enrollments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import User
+from courses.models import Course
 
-# Create your models here.
+class Enrollment(models.Model):
+    STATUS_CHOICES = (
+        ('active', 'Active'),
+        ('completed', 'Completed'),
+        ('dropped', 'Dropped'),
+    )
+
+    student = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'role': 'student'})
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='active')
+    progress = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)  # % completion
+    enrolled_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student.username} → {self.course.title} ({self.status})"
