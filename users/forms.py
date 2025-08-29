8c29d5d9cc2048340f12aa0a762ea4258f4220a9
antony43249 <odadiomoiti@gmail.com>
2025-08-29 20:54:45 +0300

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,1 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import User
 
+
+class UserRegisterForm(UserCreationForm):
+    role = forms.ChoiceField(choices=User.ROLE_CHOICES)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'role', 'age', 'password1', 'password2']
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label="Username", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
