e23b6931cd16dad1804a150daa52b83b313df104
antony43249 <odadiomoiti@gmail.com>
2025-08-29 20:45:13 +0300

    Update admin.py

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Course
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'level', 'age_group_min', 'age_group_max', 'fee', 'mode', 'created_at')
+    list_filter = ('level', 'mode')
+    search_fields = ('title', 'description')
