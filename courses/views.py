65a2c5505cdbd42559c9ec4e4f3df124e48c97e1
antony43249 <odadiomoiti@gmail.com>
2025-09-01 13:19:53 +0300

    Update views.py

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
-def users_home(request):
-    return HttpResponse("<h2>Users App Home</h2><p>Manage authentication and profiles here.</p>")
+def courses_home(request):
+    return HttpResponse("<h2>Courses App Home</h2><p>View and manage courses here.</p>")
