9aa45cecad92d47c359894171d4dcd7103ebe3b8
antony43249 <odadiomoiti@gmail.com>
2025-09-10 07:26:17 +0000

    home template on courses app initial commit

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,5 @@
+from django.shortcurt import render
 from django.http import HttpResponse
 
 def courses_home(request):
-    return HttpResponse("<h2>Courses App Home</h2><p>View and manage courses here.</p>")
+    return render(request, "home.html")
