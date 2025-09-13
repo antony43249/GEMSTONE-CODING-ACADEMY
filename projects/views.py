89ca7712677c8e382ceb6e46046215f9483f10f6
antony43249 <odadiomoiti@gmail.com>
2025-09-13 19:54:37 +0300

    Update views.py

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,5 @@
+from django.shortcut import render
 from django.http import HttpResponse
 
 def projects_home(request):
-    return HttpResponse("<h2>Projects App Home</h2><p>Student project submissions live here.</p>")
+    return render(request, 'projects/home.html')
