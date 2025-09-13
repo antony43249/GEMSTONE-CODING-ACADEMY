9235e58b6294e7e1c5425f6f4162cbb3239bae2b
antony43249 <odadiomoiti@gmail.com>
2025-09-13 20:06:56 +0300

    Update views.py

--- payments/views.py
+++ payments/views.py
@@ -1,4 +1,5 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def payments_home(request):
-    return HttpResponse("<h2>Payments App Home</h2><p>Manage tuition and sponsorship payments here.</p>")
+    return render(request, 'payments/home.html')
