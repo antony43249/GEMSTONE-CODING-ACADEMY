921c6e8811e18546512139eb993883bebbee9d24
antony43249 <odadiomoiti@gmail.com>
2025-09-13 21:19:40 +0300

    Update urls.py

--- gemstone_academy/urls.py
+++ gemstone_academy/urls.py
@@ -29,4 +29,7 @@ urlpatterns = [
     path("projects/", include("projects.urls")),
     path("payments/", include("payments.urls")),
     path("marketing/", include("marketing.urls")),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
