3aafa92b443be804d17e0c173dedb88dd99a98f5
antony43249 <odadiomoiti@gmail.com>
2025-09-13 20:59:02 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class User(AbstractUser):
     ROLE_CHOICES = (
@@ -14,3 +16,19 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.username} ({self.role})"
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    avatar = models.ImageField(upload_to="avatars/", default="avatars/default.png")
+    role = models.CharField(max_length=50, default="Student")  # e.g., Student, Instructor, Admin
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
+# Auto-create/update profile when user is created
+@receiver(post_save, sender=User)
+def create_or_update_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
