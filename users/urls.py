906c2efc5e83163dc1729496313c88e9cb37ee88
antony43249 <odadiomoiti@gmail.com>
2025-09-13 21:08:59 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("register/", register_view, name="register"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
+    path("profile/", views.profile_view, name="user-profile"),
 ]
